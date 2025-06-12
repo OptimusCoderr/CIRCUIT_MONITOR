3d64ad8ae194e89082f9f16b18044a6f3cb0d77b
Anulunko Chukwuebuka Oliver <chukwuebukaanulunko@gmail.com>
2025-06-12 22:58:11 +0100

    Update auth.routes.js

--- backend/src/routes/authRoutes/auth.routes.js
+++ backend/src/routes/authRoutes/auth.routes.js
@@ -4,13 +4,9 @@ import { protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup)
-
-router.post("/login", login)
-
-router.post("/logout", logout)
-
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 router.get("/check-auth", protectRoute, checkAuth);
-
 
 export default router;
